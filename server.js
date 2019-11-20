26bb6eef235b586b1c5473e7d569c735c43e37c0
Andy March <mail@andymarch.co.uk>
2019-11-20 09:34:44 +0000

    Swapping includes polyfill for MDN version

--- server.js
+++ server.js
@@ -13,25 +13,15 @@ app.use(history({
 app.use(serveStatic(__dirname + "/dist"));
 
 //IE11 polyfill for includes
-if (!Object.is) {
-    // eslint-disable-next-line no-extend-native
-    Object.is = function is(x, y) {
-      if (x === y) {
-        return x !== 0 || 1 / x === 1 / y;
-      }
-      return x !== x && y !== y; // eslint-disable-line no-self-compare
-    };
-  }
-  
-  if (!Array.prototype.includes) {
-    // eslint-disable-next-line no-extend-native
-    Array.prototype.includes = function includes(value) {
-      for (let i = 0; i < this.length; i += 1) {
-        if (Object.is(this[i], value)) {
-          return true;
-        }
-      }
-      return false;
+if (!String.prototype.includes) {
+    String.prototype.includes = function(search, start) {
+      'use strict';
+    console.log("IE11 polyfill")
+      if (search instanceof RegExp) {
+        throw TypeError('first argument must not be a RegExp');
+      } 
+      if (start === undefined) { start = 0; }
+      return this.indexOf(search, start) !== -1;
     };
   }
 //end polyfill
