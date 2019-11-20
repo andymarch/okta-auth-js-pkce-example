fa63a58c7add852073d55f122ff59bc949c4f3fc
Andy March <mail@andymarch.co.uk>
2019-11-20 10:01:24 +0000

    testing npm polyfill for includes

--- server.js
+++ server.js
@@ -12,24 +12,6 @@ app.use(history({
 }));
 app.use(serveStatic(__dirname + "/dist"));
 
-//IE11 polyfill for includes
-if (!String.prototype.includes) {
-    console.log("need includes polyfill")
-    String.prototype.includes = function(search, start) {
-      'use strict';
-    console.log("IE11 polyfill")
-      if (search instanceof RegExp) {
-        throw TypeError('first argument must not be a RegExp');
-      } 
-      if (start === undefined) { start = 0; }
-      return this.indexOf(search, start) !== -1;
-    };
-  }
-  else{
-      console.log("includes polyfill not needed")
-  }
-//end polyfill
-
 var port = process.env.PORT || 8080;
 app.listen(port);
 
