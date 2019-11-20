6bf8e64472c4cc6d36edf463ecf2f5d709dbf930
Andy March <mail@andymarch.co.uk>
2019-11-20 09:15:37 +0000

    Adding polyfill for includes

--- server.js
+++ server.js
@@ -12,6 +12,30 @@ app.use(history({
 }));
 app.use(serveStatic(__dirname + "/dist"));
 
+//IE11 polyfill for includes
+if (!Object.is) {
+    // eslint-disable-next-line no-extend-native
+    Object.is = function is(x, y) {
+      if (x === y) {
+        return x !== 0 || 1 / x === 1 / y;
+      }
+      return x !== x && y !== y; // eslint-disable-line no-self-compare
+    };
+  }
+  
+  if (!Array.prototype.includes) {
+    // eslint-disable-next-line no-extend-native
+    Array.prototype.includes = function includes(value) {
+      for (let i = 0; i < this.length; i += 1) {
+        if (Object.is(this[i], value)) {
+          return true;
+        }
+      }
+      return false;
+    };
+  }
+//end polyfill
+
 var port = process.env.PORT || 8080;
 app.listen(port);
 
